0f3cc6d083760108c4d04191a6cfa986eebbe48d
gitxhub05 <gitxhub1@gmail.com>
2024-10-16 19:21:40 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7812379281:AAH7r-Mw6wtGGZCt7v4C8GOlkAiGMvi-Ys4')
 
 # Admin user IDs
-admin_id = ["5894556386"]
+admin_id = ["8033268359"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
